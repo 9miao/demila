62c5adffc87f943c478a8621528c54d68099ff45
chenst <296328999@qq.com>
2015-04-01 20:29:05 +0800

    修复

--- Demila1.0Beta/apps/categories/admin/admin_config.php
+++ Demila1.0Beta/apps/categories/admin/admin_config.php
@@ -8,6 +8,15 @@
 // +----------------------------------------------------------------------
 // | Email author@demila.org
 // +----------------------------------------------------------------------
+//用户数量
+require_once ROOT_PATH.'/apps/users/models/users.class.php';
+$usersClass = new users();
+abr('usersCount', $usersClass->getUsersCount(" `status` = 'activate' "));
+
+//作品数量
+require_once ROOT_PATH.'/apps/items/models/items.class.php';
+$itemsClass = new items();
+abr('itemsCount', $itemsClass->getItemsCount());
 
 $admin_config = array (
 	'show' => true,
